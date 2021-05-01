0e85f4491cb1cb72c4fc34bc460296bcc773d15a
amithvikram rangarajan <39220547+quantumdolphin@users.noreply.github.com>
2021-05-01 12:37:17 -0700

    Add files via upload
    this is the shape screen input file.

---
+++ NCI-shape-screen-untyped-.sh
@@ -0,0 +1,1 @@
+"${SCHRODINGER}/shape_screen" -shape NCI-shape-screen-untyped-_template.mae -screen NCI-Open_2012-05-01.sdf.gz -JOB NCI-shape-screen-untyped- -flex -max 50 -filter 0.7 -sort -keep 10000 -HOST "localhost:28" -TMPLAUNCHDIR
